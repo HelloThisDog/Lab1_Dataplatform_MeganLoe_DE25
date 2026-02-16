94f33e5c27576b799d598e92eb07ae54ac0a93dc
Megan Loe <meganhelenaloe@gmail.com>
2026-02-16 14:23:23 +0100

    progress has been made, finally

--- work please.pyi
+++ work please.pyi
@@ -1,2 +1,34 @@
 import pandas as pd
 
+if __name__ == "__main__":
+
+    df = pd.DataFrame(pd.read_csv("lab1_data.csv", sep=";"))
+
+    df["id missing"] = df["id"].isna()
+    df["name missing"] = df["name"].isna()
+    df["price missing"] = df["price"].isna()
+    df["currency missing"] = df["currency"].isna()
+    df["created_at missing"] = df["created_at"].isna()
+
+    df["name"] = df["name"].astype(str).str.strip()
+    df["currency"] = df["currency"].str.strip()
+    
+    
+
+    
+    print(df)
+
+    reject = (
+        (df["id"] == ""),
+        (df["name"] == ""),
+        (df["currency"] == ""),
+        (df["created_at"] == ""),
+        (df["price"] == ""),
+        (df["price"] <= 0),
+        (df["price"] == str)
+    )
+
+    df_reject = df[reject].copy()
+    df_valid = df[~reject].copy()
+
+    df_valid.to_csv("fixed.csv", index=False)
