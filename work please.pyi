f06c1247451f48eee07700aa8890dceeb641b093
Megan Loe <meganhelenaloe@gmail.com>
2026-02-19 13:11:45 +0100

    I give up, its done, readme coming soon

--- work please.pyi
+++ work please.pyi
@@ -3,22 +3,27 @@ import pandas as pd
 if __name__ == "__main__":
 
     df = pd.DataFrame(pd.read_csv("lab1_data.csv", sep=";"))
-
+    
     df["price"] = pd.to_numeric(df["price"], errors="coerce")
 
 
     df["name"] = df["name"].astype(str).str.strip()
     df["currency"] = df["currency"].str.strip()
+
+    df["median_price"] = df["price"].median()
+    df["average_price"] = df["price"].mean()
+    df["amount"] = df["price"].count() #counts the correct amount 50 out of 55 rows
     
+    df.to_csv("analytics_summary.csv") #putting this here so the flags for the nulls don't get in the way
+
+    #flags for null values
     df["id missing"] = df["id"].isna()
     df["name missing"] = df["name"].isna()
     df["price missing"] = df["price"].isna()
     df["currency missing"] = df["currency"].isna()
     df["created_at missing"] = df["created_at"].isna()
-
     
-    print(df)
-
+    #the net to catch all of the null values and anything under zero
     reject_df = (
         (df["id missing"] == True)|
         (df["name missing"] == True)|
@@ -29,12 +34,17 @@ if __name__ == "__main__":
     )
 
 
-
+  
     bad_df = df[reject_df].copy()
     good_df = df[~reject_df].copy()
+    
 
     good_df.to_csv("fixed.csv")
 
     bad_df.to_csv("rejected_products.csv")
 
-    #analytics_summary.csv
+
+
+
+
+
