e4b09bde7cae1071778f2fb8fb447e9dfbbcc8a7
Megan Loe <meganhelenaloe@gmail.com>
2026-02-19 10:53:53 +0100

    all conditions met, flags moved

--- work please.pyi
+++ work please.pyi
@@ -4,18 +4,16 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(pd.read_csv("lab1_data.csv", sep=";"))
 
-    df["id missing"] = df["id"].isna()
-    df["name missing"] = df["name"].isna()
-    df["price missing"] = df["price"].isna()
-    df["currency missing"] = df["currency"].isna()
-    df["created_at missing"] = df["created_at"].isna()
-
     df["price"] = pd.to_numeric(df["price"], errors="coerce")
 
     df["name"] = df["name"].astype(str).str.strip()
     df["currency"] = df["currency"].str.strip()
     
-    
+    df["id missing"] = df["id"].isna()
+    df["name missing"] = df["name"].isna()
+    df["price missing"] = df["price"].isna()
+    df["currency missing"] = df["currency"].isna()
+    df["created_at missing"] = df["created_at"].isna()
 
     
     print(df)
@@ -25,9 +23,11 @@ if __name__ == "__main__":
         (df["name missing"] == True)|
         (df["currency missing"] == True)|
         (df["created_at missing"] == True)|
-        (df["price missing"] == True)
+        (df["price missing"] == True)|
+        (df["price"] <= 0)
     )
 
+    
     bad_df = df[reject_df].copy()
     good_df = df[~reject_df].copy()
 
